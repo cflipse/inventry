1189b6ab61e2971d4ac421ce22066da42b65d4e8
Chris Flipse <cflipse@gmail.com>
2020-12-05 14:40:59 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Location.find_or_create_by!(room: "Utility", name: "Freezer")
+Location.find_or_create_by!(room: "Kitchen", name: "Freezer")
+
+Location.find_or_create_by!(room: "Kitchen", name: "Right Pantry")
+Location.find_or_create_by!(room: "Kitchen", name: "Left Pantry")
+Location.find_or_create_by!(room: "Kitchen", name: "Refrigerator")
+
+Location.find_or_create_by!(room: "Utility", name: "Rolling Rack")
+Location.find_or_create_by!(room: "Utility", name: "Wall Rack")
+Location.find_or_create_by!(room: "Utility", name: "Cabinets")
