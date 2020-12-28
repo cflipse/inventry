9f2b209ebbf50f24b5f89010ae91445ce0d78935
Chris Flipse <cflipse@gmail.com>
2020-12-28 15:18:13 -0500

    fix rubocop surfaced warnings

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,7 +2,7 @@ class Food < ApplicationRecord
   validates :name, presence: true
   validates :category, presence: true
 
-  has_many :packages
+  has_many :packages, dependent: :destroy
 
   scope :in_stock, -> { where id: Package.distinct.select(:food_id) }
 end
