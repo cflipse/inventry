5663d90432d34d562daaf9034b3132490e38b0e9
Chris Flipse <cflipse@gmail.com>
2020-12-31 11:54:51 -0500

    add index view showing all room / containers

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,13 @@
 class RoomsController < ApplicationController
+  def index
+    @foods = Food.in_stock.order(:category, :name)
+    @counts = Package.food_counts
+
+    @locations = Location.includes(packages: :food)
+  end
+
   def show
     @locations = Location.where(room: params[:id])
-      .includes(:packages)
+      .includes(packages: :food)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   resources :locations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root "foods#index"
-  resources :rooms, only: [:show]
+  root "rooms#index"
+  resources :rooms, only: [:show, :index]
 
   get "/inbox", to: "packages#new"
 end
